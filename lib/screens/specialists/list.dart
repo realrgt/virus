622a5150d6aa6829797859a254fa3ffa93696f05
realrgt <ergitorubildo@gmail.com>
2020-03-16 09:55:04 +0200

    list bug fix

--- lib/screens/specialists/list.dart
+++ lib/screens/specialists/list.dart
@@ -12,154 +12,146 @@ class _SpecialistListState extends State<SpecialistList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            // TODO: remove this (All about title)
-            SizedBox(height: 40.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(16.0),
-                  width: 195.0,
-                  child: Text(
-                    'Especialistas sugeridos',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 28.0,
-                    ),
-                  ),
-                ),
-              ],
+      body: ListView(
+        children: <Widget>[
+          SizedBox(height: 20.0),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 120.0),
+            child: Text(
+              'Especialistas Sugeridos',
+              style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
             ),
-            Expanded(
-              child: ListView.builder(
-                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                itemCount: specialists.length,
-                itemBuilder: (context, index) {
-                  Specialist specialist = specialists[index];
-                  return GestureDetector(
-                    onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => SpecialistDetails(
-                          specialist: specialist,
-                        ),
+          ),
+          SizedBox(height: 20.0),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5.0),
+            child: ListView.builder(
+              shrinkWrap: true,
+              physics: ClampingScrollPhysics(),
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              itemCount: specialists.length,
+              itemBuilder: (context, index) {
+                Specialist specialist = specialists[index];
+                return GestureDetector(
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => SpecialistDetails(
+                        specialist: specialist,
                       ),
                     ),
-                    child: Stack(
-                      children: <Widget>[
-                        Container(
-                          margin: EdgeInsets.only(bottom: 20.0),
-                          height: 160.0,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20.0),
-                            color: Colors.purple,
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 25.0,
-                              vertical: 15.0,
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Padding(
-                                  padding: EdgeInsets.only(top: 5),
-                                  child: Text(
-                                    specialist.name,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 22.0,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                                Row(
-                                  children: <Widget>[
-                                    Icon(
-                                      Icons.location_on,
-                                      color: Colors.white70,
-                                    ),
-                                    Text(
-                                      specialist.address,
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 12,
-                                        color: Colors.white70,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  children: <Widget>[
-                                    Icon(
-                                      Icons.star,
-                                      color: Colors.white70,
-                                    ),
-                                    Icon(
-                                      Icons.star,
-                                      color: Colors.white70,
-                                    ),
-                                    Icon(
-                                      Icons.star,
-                                      color: Colors.white70,
-                                    ),
-                                    Icon(
-                                      Icons.star,
-                                      color: Colors.white70,
-                                    ),
-                                    Icon(
-                                      Icons.star_border,
-                                      color: Colors.white70,
-                                    ),
-                                  ],
-                                ),
-//                              _buildRatingStars(specialist.rating),
-                                Container(
-                                  padding: EdgeInsets.symmetric(
-                                    vertical: 5.0,
-                                    horizontal: 40.0,
-                                  ),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(12.0),
-                                    color: Colors.purpleAccent,
-                                  ),
-                                  child: Text(
-                                    'Ver Mais',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
+                  ),
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(bottom: 20.0),
+                        height: 160.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.purple,
                         ),
-                        Positioned(
-                          top: 0.0,
-                          right: 0.0,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20.0),
-                            child: Hero(
-                              tag: specialist.imgUrl,
-                              child: Image.asset(
-                                specialist.imgUrl,
-                                height: 160.0,
-                                width: 140.0,
-                                fit: BoxFit.cover,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 25.0,
+                            vertical: 15.0,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.only(top: 5),
+                                child: Text(
+                                  specialist.name,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 22.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
                               ),
+                              Row(
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.location_on,
+                                    color: Colors.white70,
+                                  ),
+                                  Text(
+                                    specialist.address,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12,
+                                      color: Colors.white70,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.star,
+                                    color: Colors.white70,
+                                  ),
+                                  Icon(
+                                    Icons.star,
+                                    color: Colors.white70,
+                                  ),
+                                  Icon(
+                                    Icons.star,
+                                    color: Colors.white70,
+                                  ),
+                                  Icon(
+                                    Icons.star,
+                                    color: Colors.white70,
+                                  ),
+                                  Icon(
+                                    Icons.star_border,
+                                    color: Colors.white70,
+                                  ),
+                                ],
+                              ),
+//                              _buildRatingStars(specialist.rating),
+                              Container(
+                                padding: EdgeInsets.symmetric(
+                                  vertical: 5.0,
+                                  horizontal: 40.0,
+                                ),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                  color: Colors.purpleAccent,
+                                ),
+                                child: Text(
+                                  'Ver Mais',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 0.0,
+                        right: 0.0,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20.0),
+                          child: Hero(
+                            tag: specialist.imgUrl,
+                            child: Image.asset(
+                              specialist.imgUrl,
+                              height: 160.0,
+                              width: 140.0,
+                              fit: BoxFit.cover,
                             ),
                           ),
                         ),
-                      ],
-                    ),
-                  );
-                },
-              ),
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
