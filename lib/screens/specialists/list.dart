c123919453d0482abfdb8787ab0855f31e623904
realrgt <ergitorubildo@gmail.com>
2020-05-23 16:28:24 +0200

    spinkit loading

--- lib/screens/specialists/list.dart
+++ lib/screens/specialists/list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 import 'package:vogu/core/contollers/specialist-crud.dart';
 import 'package:vogu/core/models/specialist.dart';
@@ -52,18 +53,21 @@ class _SpecialistListState extends State<SpecialistList> {
       body: ListView(
         children: <Widget>[
           SizedBox(height: 20.0),
-          Stack(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0, right: 120.0),
-                child: Text(
-                  'Especialistas Sugeridos',
-                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-                ),
-              ),
-              CrossIcon(color: Colors.black26, paddingRight: 16.0),
-            ],
-          ),
+          specialists.length > 0
+              ? Stack(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0, right: 120.0),
+                      child: Text(
+                        'Especialistas Sugeridos',
+                        style: TextStyle(
+                            fontSize: 30.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    CrossIcon(color: Colors.black26, paddingRight: 16.0),
+                  ],
+                )
+              : Container(),
           SizedBox(height: 20.0),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 5.0),
@@ -104,135 +108,153 @@ class _SpecialistListState extends State<SpecialistList> {
                   print(specialists.length);
                   print('DEBUG ============== ==>');
 
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    physics: ClampingScrollPhysics(),
-                    padding: EdgeInsets.symmetric(horizontal: 16.0),
-                    itemCount: specialists.length,
-                    itemBuilder: (context, index) {
-                      Specialist specialist = specialists.elementAt(index);
-                      return GestureDetector(
-                        onTap: () {
-                          /// set [specialistId] to task provider
-                          taskInfo.specialistId =
-                              specialists.elementAt(index).id;
+                  if (specialists.length > 0) {
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      physics: ClampingScrollPhysics(),
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      itemCount: specialists.length,
+                      itemBuilder: (context, index) {
+                        Specialist specialist = specialists.elementAt(index);
+                        return GestureDetector(
+                          onTap: () {
+                            /// set [specialistId] to task provider
+                            taskInfo.specialistId =
+                                specialists.elementAt(index).id;
 
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (_) => SpecialistDetails(
-                                specialist: specialists.elementAt(index),
-                              ),
-                            ),
-                          );
-                        },
-                        child: Stack(
-                          children: <Widget>[
-                            Container(
-                              margin: EdgeInsets.only(bottom: 20.0),
-                              height: 160.0,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20.0),
-                                color: PURPLE_DEEP,
-                              ),
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                  horizontal: 25.0,
-                                  vertical: 15.0,
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (_) => SpecialistDetails(
+                                  specialist: specialists.elementAt(index),
                                 ),
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Padding(
-                                      padding: EdgeInsets.only(top: 5),
-                                      child: Text(
-                                        specialist.name,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 22.0,
-                                          fontWeight: FontWeight.bold,
+                              ),
+                            );
+                          },
+                          child: Stack(
+                            children: <Widget>[
+                              Container(
+                                margin: EdgeInsets.only(bottom: 20.0),
+                                height: 160.0,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  color: PURPLE_DEEP,
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 25.0,
+                                    vertical: 15.0,
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: EdgeInsets.only(top: 5),
+                                        child: Text(
+                                          specialist.name,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 22.0,
+                                            fontWeight: FontWeight.bold,
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                    Row(
-                                      children: <Widget>[
-                                        Icon(
-                                          Icons.location_on,
-                                          color: Colors.white70,
-                                        ),
-                                        Text(
-                                          specialist.address,
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 12,
+                                      Row(
+                                        children: <Widget>[
+                                          Icon(
+                                            Icons.location_on,
                                             color: Colors.white70,
                                           ),
-                                        ),
-                                      ],
-                                    ),
-                                    Row(
-                                      children: <Widget>[
-                                        RatingBarIndicator(
-                                          rating: specialist.rating,
-                                          itemBuilder: (context, index) => Icon(
-                                            Icons.star,
-                                            color: Colors.amber,
+                                          Text(
+                                            specialist.address,
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 12,
+                                              color: Colors.white70,
+                                            ),
                                           ),
-                                          itemCount: 5,
-                                          itemSize: 20.0,
-                                          direction: Axis.horizontal,
-                                        ),
-                                      ],
-                                    ),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: <Widget>[
+                                          RatingBarIndicator(
+                                            rating: specialist.rating,
+                                            itemBuilder: (context, index) =>
+                                                Icon(
+                                              Icons.star,
+                                              color: Colors.amber,
+                                            ),
+                                            itemCount: 5,
+                                            itemSize: 20.0,
+                                            direction: Axis.horizontal,
+                                          ),
+                                        ],
+                                      ),
 //                              _buildRatingStars(specialist.rating),
-                                    Container(
-                                      padding: EdgeInsets.symmetric(
-                                        vertical: 5.0,
-                                        horizontal: 40.0,
+                                      Container(
+                                        padding: EdgeInsets.symmetric(
+                                          vertical: 5.0,
+                                          horizontal: 40.0,
+                                        ),
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(12.0),
+                                          color: PURPLE_ACCENT,
+                                        ),
+                                        child: Text(
+                                          'Ver Mais',
+                                          style: TextStyle(color: Colors.white),
+                                        ),
                                       ),
-                                      decoration: BoxDecoration(
-                                        borderRadius:
-                                            BorderRadius.circular(12.0),
-                                        color: PURPLE_ACCENT,
-                                      ),
-                                      child: Text(
-                                        'Ver Mais',
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                              top: 0.0,
-                              right: 0.0,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(20.0),
-                                child: Hero(
-                                  tag: specialist.imgUrl,
-                                  child: Image.network(
-                                    '${specialist.imgUrl}',
-                                    height: 160.0,
-                                    width: 140.0,
-                                    fit: BoxFit.cover,
+                                    ],
                                   ),
                                 ),
                               ),
-                            ),
-                          ],
+                              Positioned(
+                                top: 0.0,
+                                right: 0.0,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  child: Hero(
+                                    tag: specialist.imgUrl,
+                                    child: Image.network(
+                                      '${specialist.imgUrl}',
+                                      height: 160.0,
+                                      width: 140.0,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return Container(
+                      height: MediaQuery.of(context).size.height * 0.7,
+                      child: Center(
+                        child: Text(
+                          'No specialist found for \nprovided services!!',
+                          style: TextStyle(
+                            fontSize: 28.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          textAlign: TextAlign.center,
                         ),
-                      );
-                    },
-                  );
+                      ),
+                    );
+                  }
                 } else {
-                  return Center(
-                    child: Text(
-                      'Fetching data',
-                      style: TextStyle(
-                        fontSize: 28.0,
-                        fontWeight: FontWeight.w600,
+                  return Container(
+                    height: MediaQuery.of(context).size.height * 0.7,
+                    child: Center(
+                      child: SpinKitFadingCircle(
+                        color: PURPLE_DEEP,
+                        size: 50.0,
                       ),
                     ),
                   );
