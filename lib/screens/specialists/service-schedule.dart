a341f2996601fed8ef2886856ae1d164c81a35b5
realrgt <ergitorubildo@gmail.com>
2020-05-23 14:01:04 +0200

    length validation & title toggle

--- lib/screens/specialists/service-schedule.dart
+++ lib/screens/specialists/service-schedule.dart
@@ -31,7 +31,6 @@ class _ServiceScheduleState extends State<ServiceSchedule> {
 
   @override
   Widget build(BuildContext context) {
-
     ///try provider
     var taskInfo = Provider.of<Task>(context);
     var itemCount = 0;
@@ -59,37 +58,46 @@ class _ServiceScheduleState extends State<ServiceSchedule> {
             SizedBox(height: 20),
             Padding(
               padding: EdgeInsets.only(left: 32.0, right: 32.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    "Selecione os serviços",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 28.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  Container(
-                    height: 35.0,
-                    width: 35.0,
-                    decoration: BoxDecoration(
-                      color: BLUE,
-                      shape: BoxShape.circle,
-                    ),
-                    child: Center(
-                      child: Text(
-                        '$itemCount',
-                        style: TextStyle(
-                          color: PINK,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
+              child: itemCount > 0
+                  ? Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          "Serviços selecionados",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28.0,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
+                        Container(
+                          height: 35.0,
+                          width: 35.0,
+                          decoration: BoxDecoration(
+                            color: BLUE,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: Text(
+                              '$itemCount',
+                              style: TextStyle(
+                                color: PINK,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  : Text(
+                      "Selecione os serviços",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
-                  ),
-                ],
-              ),
             ),
             SizedBox(height: 15.0),
             Expanded(
@@ -133,7 +141,6 @@ class _ServiceScheduleState extends State<ServiceSchedule> {
                                     setState(() {
                                       _formatDate(datePicked);
                                     });
-
                                   },
                                   child: Container(
                                     width: 150.0,
@@ -179,7 +186,6 @@ class _ServiceScheduleState extends State<ServiceSchedule> {
                                           .toString()
                                           .substring(10, 15);
                                     });
-
                                   },
                                   child: Container(
                                     width: 150.0,
@@ -215,19 +221,25 @@ class _ServiceScheduleState extends State<ServiceSchedule> {
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.stretch,
                               children: <Widget>[
-                                GestureDetector(
-                                  onTap: () => Navigator.of(context).push(
-                                    MaterialPageRoute(
-                                      builder: (_) => SpecialistList(),
-                                    ),
-                                  ),
+                                InkWell(
+                                  onTap: () {
+                                    itemCount > 0
+                                        ? Navigator.of(context).push(
+                                            MaterialPageRoute(
+                                              builder: (_) => SpecialistList(),
+                                            ),
+                                          )
+                                        : print('Select a service');
+                                  },
                                   child: Container(
                                     width: 145.0,
                                     height: 47.0,
                                     padding:
                                         EdgeInsets.symmetric(horizontal: 20.0),
                                     decoration: BoxDecoration(
-                                      color: PURPLE_DEEP,
+                                      color: itemCount > 0
+                                          ? PURPLE_DEEP
+                                          : PURPLE_DEEP.withOpacity(0.7),
                                       borderRadius: BorderRadius.circular(15.0),
                                     ),
                                     child: Row(
